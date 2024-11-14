e5e1a86e0b11bdca49b3935a9f0f75729a62490c
Rishabh Bilwal <88207344+rishabhbilwal2203@users.noreply.github.com>
2024-11-14 16:46:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/api', require('./routes/api'));
 
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
